732abae4fb158eb7383f46cdeb3f6b18309f7314
Caio Brighenti <caio.brig@gmail.com>
2019-01-16 01:46:27 -0200

    Add per game stats to crawler, improve R data import

--- MVPTrack.R
+++ MVPTrack.R
@@ -1,71 +1,51 @@
-#### MAIN DATA ####
-# GRAB DATA FROM EACH SEASON
-main_dat<-data.frame(Rk=integer(),Player=character(),Pos=character(),Age=double(),Tm=character(),
-                    G=double(),GS=double(),MP=double(),FG=double(),FGA=double(),FG.=double(),
-                    X3P=double(),X3PA=double(),X3P.=double(),X2P=double(),X2PA=double(),
-                    X2P.=double(),eFG.=double(),FT=double(),FTA=double(),FT.=double(),ORB=double(),
-                    DRB=double(),TRB=double(),AST=double(),STL=double(),BLK=double(),TOV=double(),
-                    PF=double(),PTS=double(),Season=integer())
-for (year in 1976:2017) {
-  # read data
-  str<-paste("C:/Users/Caio/repos/nba-models/season-stats/",year,".csv",sep="")
-  temp_dat<-read.csv(str, header = TRUE)
-  
-  # add season column
-  temp_dat$Season<-year
-  
-  # clean player names
-  temp_dat$Player<-as.character(temp_dat$Player)
-  names<-strsplit(as.character(temp_dat$Player),"[\\\\]")
-  for (idx in 1:dim(temp_dat)[1]) {
-    temp_dat$Player[idx]<-names[[idx]][1]
-  }
-  
-  # might consider lockout seasons
-  
-  ### NEED TO ADD CODE TO CLEAN PARTIAL SEASONS
-  
-  # add season to main dataframe
-  main_dat<-data.frame(rbind(as.matrix(main_dat), as.matrix(temp_dat)))
-}
-head(main_dat)
+# constants
+YEAR_START<-2000
+YEAR_END<-2017
 
-
-#### MVP DATA ####
+#######################################################
+#################### MVP DATA ######################### 
+#######################################################
 # GRAB MVP DATA FROM EACH SEASON
 mvp_dat<-data.frame(Rank=integer(),Player=character(),Age=double(),Tm=character(),
                     First=double(),Pts.Won=double(),Pts.Max=double(),Share=double(),
                     G=double(),MP=double(),PTS=double(),TRB=double(),AST=double(),
                     STL=double(),BLK=double(),FG.=double(),X3P.=double(),FT.=double(),
                     WS=double(),WS.48=double(),Season=double())
-for (year in 2000:2017) {
+for (year in YEAR_START:YEAR_END) {
   str<-paste("C:/Users/Caio/repos/nba-models/award-stats/",year,".csv",sep="")
-  temp_dat<-read.csv(str, header = TRUE,stringsAsFactors=FALSE)
-   temp_dat$Season<-year
+  dat_temp<-read.csv(str, header = TRUE,stringsAsFactors=FALSE)
+  dat_temp$Season<-year
   # normalize stats & set class
   # Model is more accurate without this
   ## G
-  temp_dat$G<-temp_dat$G/max(temp_dat$G)
+  dat_temp$G<-dat_temp$G/max(dat_temp$G)
   ## MP
-  temp_dat$MP<-temp_dat$MP/max(temp_dat$MP)
+  dat_temp$MP<-dat_temp$MP/max(dat_temp$MP)
   ## PTS
-  temp_dat$PTS<-temp_dat$PTS/max(temp_dat$PTS)
+  dat_temp$PTS<-dat_temp$PTS/max(dat_temp$PTS)
   ## TRB
-  temp_dat$TRB<-temp_dat$TRB/max(temp_dat$TRB)
+  dat_temp$TRB<-dat_temp$TRB/max(dat_temp$TRB)
   ## AST
-  temp_dat$AST<-temp_dat$AST/max(temp_dat$AST)
+  dat_temp$AST<-dat_temp$AST/max(dat_temp$AST)
   ## STL
-  temp_dat$STL<-temp_dat$STL/max(temp_dat$STL)
+  dat_temp$STL<-dat_temp$STL/max(dat_temp$STL)
   ## BLK
-  temp_dat$BLK<-temp_dat$BLK/max(temp_dat$BLK)
+  dat_temp$BLK<-dat_temp$BLK/max(dat_temp$BLK)
   ## FG.
-  temp_dat$FG.<-temp_dat$FG./max(temp_dat$FG.)
+  dat_temp$FG.<-dat_temp$FG./max(dat_temp$FG.)
   ## FT.
-  temp_dat$FT.<-temp_dat$FT./max(temp_dat$FT.)
+  dat_temp$FT.<-dat_temp$FT./max(dat_temp$FT.)
   ## WS.48
-  temp_dat$WS.48<-temp_dat$WS.48/max(temp_dat$WS.48)
-  #names(temp_dat)<-names(mvp_dat)
-  mvp_dat<-data.frame(rbind.data.frame(as.matrix(mvp_dat), as.matrix(temp_dat)))
+  dat_temp$WS.48<-dat_temp$WS.48/max(dat_temp$WS.48)
+  
+  # clean player names
+  dat_temp$Player<-as.character(dat_temp$Player)
+  names<-strsplit(as.character(dat_temp$Player),"[\\\\]")
+  for (idx in 1:dim(dat_temp)[1]) {
+    dat_temp$Player[idx]<-names[[idx]][1]
+  }
+  
+  mvp_dat<-data.frame(rbind.data.frame(as.matrix(mvp_dat), as.matrix(dat_temp)))
 }
 head(mvp_dat)
 
@@ -98,14 +78,14 @@ summary(mvp.mod.red)
 # make predictions
 pred<-fitted(mvp.mod)
 mvps<-data.frame(Rank=integer(),Player=character(),Age=double(),Tm=character(),
-                    First=double(),Pts.Won=double(),Pts.Max=double(),Share=double(),
-                    G=double(),MP=double(),PTS=double(),TRB=double(),AST=double(),
-                    STL=double(),BLK=double(),FG.=double(),X3P.=double(),FT.=double(),
-                    WS=double(),WS.48=double(),Season=double())
+                 First=double(),Pts.Won=double(),Pts.Max=double(),Share=double(),
+                 G=double(),MP=double(),PTS=double(),TRB=double(),AST=double(),
+                 STL=double(),BLK=double(),FG.=double(),X3P.=double(),FT.=double(),
+                 WS=double(),WS.48=double(),Season=double())
 for (year in levels(as.factor(mvp_dat$Season))) {
-  temp_dat<-mvp_dat[which(mvp_dat$Season==year),]
+  dat_temp<-mvp_dat[which(mvp_dat$Season==year),]
   temp_preds<-pred[which(mvp_dat$Season==year)]
-  mvp<-temp_dat[which(temp_preds==max(temp_preds)),]
+  mvp<-dat_temp[which(temp_preds==max(temp_preds)),]
   mvps<-data.frame(rbind(as.matrix(mvps), as.matrix(mvp)))
 }
 
@@ -113,6 +93,92 @@ for (year in levels(as.factor(mvp_dat$Season))) {
 # calculate accuracy
 ## hack to fix seasons like 1995,2009,2016 where rank is messed up
 #levels(mvps$Rank)<-c(as.numeric(levels(mvps$Rank)))
+truevals<-mvp_dat[which(mvp_dat$Rank==1),]
 errs<-mvps[which(mvps$Rank!=1),]
 accuracy<-1-(dim(errs)[1]/dim(mvps)[1])
 accuracy
+
+#######################################################
+################ SEASON PLAYER TOTALS ################
+#######################################################
+# GRAB DATA FROM EACH SEASON
+dat_totals<-data.frame(Rk=integer(),Player=character(),Pos=character(),Age=double(),Tm=character(),
+                    G=double(),GS=double(),MP=double(),FG=double(),FGA=double(),FG.=double(),
+                    X3P=double(),X3PA=double(),X3P.=double(),X2P=double(),X2PA=double(),
+                    X2P.=double(),eFG.=double(),FT=double(),FTA=double(),FT.=double(),ORB=double(),
+                    DRB=double(),TRB=double(),AST=double(),STL=double(),BLK=double(),TOV=double(),
+                    PF=double(),PTS=double(),Season=integer())
+for (year in YEAR_START:YEAR_END) {
+  # read data
+  str<-paste("C:/Users/Caio/repos/nba-models/season-stats-totals/",year,".csv",sep="")
+  dat_temp<-read.csv(str, header = TRUE)
+  
+  # add season column
+  dat_temp$Season<-year
+  
+  # clean player names
+  dat_temp$Player<-as.character(dat_temp$Player)
+  names<-strsplit(as.character(dat_temp$Player),"[\\\\]")
+  for (idx in 1:dim(dat_temp)[1]) {
+    name<-names[[idx]][1]
+    name<-gsub("[*]","",name)
+    dat_temp$Player[idx]<-name
+  }
+  
+  # might consider lockout seasons
+  
+  ### NEED TO ADD CODE TO CLEAN PARTIAL SEASONS
+  
+  # add season to main dataframe
+  dat_totals<-data.frame(rbind(as.matrix(dat_totals), as.matrix(dat_temp)))
+}
+head(dat_totals)
+
+## add MVP winning seasons
+dat_totals$MVP<-FALSE
+for (idx in 1:dim(truevals)[1]) {
+  dat_totals[which(as.character(dat_totals$Player)==as.character(truevals[idx,]$Player)&dat_totals$Season==truevals[idx,]$Season),]$MVP<-TRUE
+}
+
+# add first place votes
+dat_totals$First<-0
+for (idx in 1:4) {#dim(mvp_dat)[1]) {
+  temp_row<-dat_totals[which(as.character(dat_totals$Player)==as.character(mvp_dat[idx,]$Player)&dat_totals$Season==mvp_dat[idx,]$Season),]
+  temp_row$First<-mvp_dat[idx,]$First
+}
+
+#######################################################
+################ SEASON PLAYER PER GAME################ 
+#######################################################
+# GRAB DATA FROM EACH SEASON
+dat_pg<-data.frame(Rk=integer(),Player=character(),Pos=character(),Age=double(),Tm=character(),
+                     G=double(),GS=double(),MP=double(),FG=double(),FGA=double(),FG.=double(),
+                     X3P=double(),X3PA=double(),X3P.=double(),X2P=double(),X2PA=double(),
+                     X2P.=double(),eFG.=double(),FT=double(),FTA=double(),FT.=double(),ORB=double(),
+                     DRB=double(),TRB=double(),AST=double(),STL=double(),BLK=double(),TOV=double(),
+                     PF=double(),PS.G=double(),Season=integer())
+for (year in YEAR_START:YEAR_END) {
+  # read data
+  str<-paste("C:/Users/Caio/repos/nba-models/season-stats-pergame/",year,".csv",sep="")
+  dat_temp<-read.csv(str, header = TRUE)
+  
+  # add season column
+  dat_temp$Season<-year
+  
+  # clean player names
+  dat_temp$Player<-as.character(dat_temp$Player)
+  names<-strsplit(as.character(dat_temp$Player),"[\\\\]")
+  for (idx in 1:dim(dat_temp)[1]) {
+    dat_temp$Player[idx]<-names[[idx]][1]
+  }
+  
+  # might consider lockout seasons
+  
+  ### NEED TO ADD CODE TO CLEAN PARTIAL SEASONS
+  
+  # add season to main dataframe
+  dat_pg<-data.frame(rbind(as.matrix(dat_pg), as.matrix(dat_temp)))
+}
+head(dat_pg)
+
+
--- scraper.py
+++ scraper.py
@@ -7,23 +7,45 @@ import os
 # start browser crawler
 browser = webdriver.Firefox()
 
-for season in range(1976, 2019):
+## GRAB SEASON PLAYER STAT PER GAME
+for season in range(1976, 2018):
+    # navigate to bballref
+    url = 'https://www.basketball-reference.com/leagues/NBA_' + str(season) + '_per_game.html'
+    browser.get(url)
+    # activate and grab data in CSV format
+    raw_csv = browser.execute_script('''document.getElementById("per_game_stats_toggle_partial_table").click();
+    var x = document.getElementsByClassName("tooltip");
+    x[3].click();
+    var content = document.getElementById("csv_per_game_stats")
+    return content.textContent
+    ''')
+
+    # write to CSV
+    pathstr = "season-stats-pergame/" + str(season) + ".csv"
+    f = open(pathstr, "w")
+    f.write(raw_csv)
+
+## GRAB SEASON PLAYER STAT TOTALS
+for season in range(1976, 2018):
+    season=1977
     # navigate to bballref
     url = 'https://www.basketball-reference.com/leagues/NBA_' + str(season) + '_totals.html'
     browser.get(url)
     # activate and grab data in CSV format
-    raw_csv = browser.execute_script('''var x = document.getElementsByClassName("tooltip");
+    raw_csv = browser.execute_script('''document.getElementById("totals_stats_toggle_partial_table").click();
+    var x = document.getElementsByClassName("tooltip");
     x[3].click();
     var content = document.getElementById("csv_totals_stats")
     return content.textContent
     ''')
 
     # write to CSV
-    pathstr = "season-stats/" + str(season) + ".csv"
+    pathstr = "season-stats-totals/" + str(season) + ".csv"
     f = open(pathstr, "w")
     f.write(raw_csv)
 
-for season in range(1980, 2019):
+## GRAB SEASON MVP STATS
+for season in range(1976, 1977):
     # navigate to bballref
     url = 'https://www.basketball-reference.com/awards/awards_' + str(season) + '.html'
     browser.get(url)
